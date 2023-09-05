602604b90d75c05c82408467275f1f85c7b5d84c
cletusola <olamicolo@gmail.com>
2023-09-05 00:33:15 -0700

    modified auth_app/urls.py

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 
 
 urlpatterns = [
-    path('api/auth/', include('auth.urls')),
+    path('api/auth/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
